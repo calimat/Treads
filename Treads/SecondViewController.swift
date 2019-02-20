0ed1a1ff16574cfea971ecb25f323a1b367b7129
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-02-19 20:14:30 -0500

    Initial Commit

---
+++ Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treads
+//
+//  Created by Ricardo Herrera Petit on 2/19/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
