11e88630d28edbf23065ea358479f6d7a7f64f02
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-02-24 18:23:38 -0500

    Added CurrentRunVC file

---
+++ Treads/Controller/CurrentRunVC.swift
@@ -0,0 +1,22 @@
+//
+//  CurrentRunVC.swift
+//  Treads
+//
+//  Created by Ricardo Herrera Petit on 2/24/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class CurrentRunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
