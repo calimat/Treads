e9d608e30b8b81954d86b4d2100bc30ceddcfa48
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-02-24 19:12:29 -0500

    Setup custom slider switch

--- Treads/Controller/CurrentRunVC.swift
+++ Treads/Controller/CurrentRunVC.swift
@@ -8,14 +8,46 @@
 
 import UIKit
 
-class CurrentRunVC: UIViewController {
+class CurrentRunVC: LocationVC {
 
+    
+    @IBOutlet weak var swipeBGImageView: UIImageView!
+    @IBOutlet weak var sliderImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(endRunSwiped(sender:)))
+        sliderImageView.addGestureRecognizer(swipeGesture)
+        sliderImageView.isUserInteractionEnabled = true
+        swipeGesture.delegate = self as? UIGestureRecognizerDelegate
 
-        // Do any additional setup after loading the view.
     }
     
+    @objc func endRunSwiped(sender: UIPanGestureRecognizer) {
+        let minAdjust: CGFloat = 80
+        let maxAdjust: CGFloat = 128
+        if let sliderView = sender.view {
+            if sender.state == .began || sender.state == .changed {
+                let translation = sender.translation(in: self.view)
+                if sliderView.center.x >= (swipeBGImageView.center.x - minAdjust) && sliderView.center.x <= (swipeBGImageView.center.x + maxAdjust) {
+                    sliderView.center.x = sliderView.center.x + translation.x
+                } else if sliderView.center.x >= (swipeBGImageView.center.x + maxAdjust) {
+                    sliderView.center.x = swipeBGImageView.center.x + maxAdjust
+                    //End Run Code goes here
+                    dismiss(animated: true, completion: nil)
+                } else {
+                    sliderView.center.x = swipeBGImageView.center.x - minAdjust
+                }
+                
+                sender.setTranslation(CGPoint.zero, in: self.view)
+            } else if sender.state == .ended {
+                UIView.animate(withDuration: 0.1) {
+                    sliderView.center.x = self.swipeBGImageView.center.x - minAdjust
+                }
+            }
+        }
+    }
 
     
 
