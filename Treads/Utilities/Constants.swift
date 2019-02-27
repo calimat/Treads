3979cd960ca7f7b0a09a415653a4ff0df0ece7af
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-02-26 21:52:02 -0500

    Write and fetch data

---
+++ Treads/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Treads
+//
+//  Created by Ricardo Herrera Petit on 2/26/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
+
+let REALM_QUEUE = DispatchQueue(label: "realmQueue")
