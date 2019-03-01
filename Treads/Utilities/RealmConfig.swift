d76d98d8a2a38d0ea8449edf40956c4dc03e3366
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-02-28 21:01:48 -0500

    Realm configuration

---
+++ Treads/Utilities/RealmConfig.swift
@@ -0,0 +1,23 @@
+//
+//  RealmConfig.swift
+//  Treads
+//
+//  Created by Ricardo Herrera Petit on 2/28/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmConfig {
+    static var runDataConfig: Realm.Configuration {
+        let realmPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(REALM_RUN_CONFIG)
+        let config = Realm.Configuration(fileURL: realmPath, schemaVersion:0, migrationBlock: { migration, oldSchemaVersion in
+            if(oldSchemaVersion < 0) {
+                //Nothing to do
+                //Realm will automatically detect new properties and remove properties
+            }
+        })
+        return config
+    }
+}
